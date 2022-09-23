7c685154a2377def3bf258f0f89c5765b0e9c886
Raghu Betina <raghu.betina@chicagobooth.edu>
2022-09-23 03:36:15 +0000

    Whitelist columns in Categorization for ActiveAdmin

--- app/admin/categorizations.rb
+++ app/admin/categorizations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Categorization do
 
+ permit_params :place_id, :category_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
